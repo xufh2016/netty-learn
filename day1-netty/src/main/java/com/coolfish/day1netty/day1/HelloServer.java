e39146577d8e396cb8069903754cb339677489c3
xufanghong <282516701@qq.com>
2021-05-31 10:43:55 +0800

    1

--- day1-netty/src/main/java/com/coolfish/day1netty/day1/HelloServer.java
+++ day1-netty/src/main/java/com/coolfish/day1netty/day1/HelloServer.java
@@ -1,24 +1,15 @@
 package com.coolfish.day1netty.day1;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
-import io.netty.channel.*;
-import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
-import io.netty.handler.codec.LengthFieldPrepender;
-import io.netty.handler.codec.http.*;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 import io.netty.util.CharsetUtil;
-
-import java.io.UnsupportedEncodingException;
-import java.net.SocketAddress;
-import java.nio.charset.Charset;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class HelloServer {
     public static void main(String[] args) {
@@ -47,83 +38,9 @@ public class HelloServer {
                                     pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));//StringDecoder将ByteBuf转换为字符串,解码器，客户端向服务器端传输数据时需要编码器，
                                     pipeline.addLast(new StringEncoder(CharsetUtil.UTF_8));
                                     // 当服务器端收到数据后需要使用解码器进行解码。
-                                    pipeline.addLast(new ChannelInboundHandlerAdapter() {//自定义handler
-
-                                        @Override
-                                        //读事件
-                                        public void channelRead(ChannelHandlerContext ctx, Object msg) throws UnsupportedEncodingException {
-                                            Channel channel = ctx.channel();
-                                            SocketAddress socketAddress = channel.remoteAddress();
-                                            String string = socketAddress.toString();
-
-                                            System.out.println(ctx.channel().remoteAddress() + "--------------------------------------");
-                                            String str = (String) msg;
-                                            System.out.println("执行channelRead~~~~~~~~~~~~");
-                                            System.out.println("---------------Server接收到来自客户端的消息-------------" + str);
-
-
-                                            ctx.channel().writeAndFlush("-----------server says hi---------------------");
-//                                            ByteBuf byteBuf = Unpooled.copiedBuffer("Hello , This is Server!!!", CharsetUtil.UTF_8);
-//                                            FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, byteBuf);
-//                                            response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
-//                                            response.headers().set(HttpHeaderNames.CONTENT_LENGTH, byteBuf.readableBytes());
-//                                            ctx.writeAndFlush(response);
-                                        }
-
-                                        @Override
-                                        public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
-                                            System.out.println("------------------channelRegistered---------");
-                                        }
-
-                                        @Override
-                                        public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
-                                            System.out.println("-----------channelUnregistered----------");
-                                        }
-
-                                        @Override
-                                        //该方法表示通道已连接，连接好之后会自动回调
-                                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
-                                            System.out.println("--------------channelActive------------");
-                                        }
-
-                                        @Override
-                                        public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-                                            System.out.println("----------channelInactive--------");
-                                        }
-
-                                        @Override
-                                        public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-                                            System.out.println("---------------channelReadComplete--------------");
-                                        }
-
-                                        @Override
-                                        public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
-                                            System.out.println("------------channelWritabilityChanged---------");
-                                        }
-
-                                        @Override
-                                        public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
-                                            System.out.println("---------------userEventTriggered--------------");
-                                        }
-
-                                        @Override
-                                        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-                                            System.out.println("-----------exceptionCaught----------");
-                                        }
-
-                                        @Override
-                                        public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
-                                            System.out.println("--------------handlerRemoved------------");
-                                        }
-
-                                        @Override
-                                        public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
-                                            System.out.println("---------------handlerAdded------------");
-                                        }
-                                    });
+                                    pipeline.addLast(new HelloServerHandler<NioSocketChannel>());
                                 }
                             }).bind(8081).sync();//绑定监听端口
-
             channelFuture.channel().closeFuture().sync();
         } catch (InterruptedException e) {
             e.printStackTrace();
---
+++ day1-netty/src/main/java/com/coolfish/day1netty/day1/HelloServerHandler.java
@@ -0,0 +1,94 @@
+package com.coolfish.day1netty.day1;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+import java.io.UnsupportedEncodingException;
+import java.net.SocketAddress;
+
+public class HelloServerHandler<NioSocketChannel> extends ChannelInboundHandlerAdapter {
+    private static ChannelGroup channelGroup = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
+    @Override
+    //读事件
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws UnsupportedEncodingException {
+        Channel channel = ctx.channel();
+        SocketAddress socketAddress = channel.remoteAddress();
+        String string = socketAddress.toString();
+
+        System.out.println(ctx.channel().remoteAddress() + "--------------------------------------");
+        String str = (String) msg;
+        System.out.println("执行channelRead~~~~~~~~~~~~");
+        System.out.println("---------------Server接收到来自客户端的消息-------------" + str);
+
+
+        ctx.channel().writeAndFlush("-----------server says hi---------------------" + ctx.channel().remoteAddress());
+//                                            ByteBuf byteBuf = Unpooled.copiedBuffer("Hello , This is Server!!!", CharsetUtil.UTF_8);
+//                                            FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, byteBuf);
+//                                            response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
+//                                            response.headers().set(HttpHeaderNames.CONTENT_LENGTH, byteBuf.readableBytes());
+//                                            ctx.writeAndFlush(response);
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("------------------channelRegistered---------");
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("-----------channelUnregistered----------");
+    }
+
+    @Override
+    //该方法表示通道已连接，连接好之后会自动回调
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+        channelGroup.writeAndFlush("---------------------[Server]-"+channel.remoteAddress()+"----------join in----------");
+
+        channelGroup.add(channel);
+        System.out.println("--------------channelActive------------");
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("----------channelInactive--------");
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("---------------channelReadComplete--------------");
+        Channel channel = ctx.channel();
+        channelGroup.writeAndFlush("[Server]-"+channel.remoteAddress()+"-----left----");
+        channelGroup.remove(channel);
+    }
+
+    @Override
+    public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("------------channelWritabilityChanged---------");
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        System.out.println("---------------userEventTriggered--------------");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println("-----------exceptionCaught----------");
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("--------------handlerRemoved------------");
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("---------------handlerAdded------------");
+    }
+}
